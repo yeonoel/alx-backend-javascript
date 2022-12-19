a8fa1bac6652aeac99793d6f434417bb9408dc30
yeonoel <yeopevroguinoel@gmail.com>
2022-12-19 16:14:06 +0000

    Modify function

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  var task = true;
-  var task2 = false;
 
   if (trueOrFalse) {
     task = false;
     task2 = true;
   }
-
+  var task = false;
+  var task2 = true;
   return [task, task2];
 }
