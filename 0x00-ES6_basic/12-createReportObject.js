377cc35ca6b546d74cf89741137d1e8d48472ab5
yeonoel <yeopevroguinoel@gmail.com>
2022-12-20 14:47:27 +0000

    Let's create a report object

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,9 +1,8 @@
 export default function createReportObject(employeesList) {
-  object = { allEmployees: {...employeesList,},};
-
-  getNumberOfDepartement(empList) {
-    return Object.keys(empList).length;
-  }
-
-  return object;
+  return {
+    allEmployees: {...employeesList,},
+    getNumberOfDepartement(empList) {
+      return Object.keys(empList).length;
+    },
+  };
 };
