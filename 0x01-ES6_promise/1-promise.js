85bccff020735e2274de659fa4a72171f4010457
yeonoel <yeopevroguinoel@gmail.com>
2022-12-21 15:32:11 +0000

    functiion to return a promise

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,16 @@
+export default function getFullResponseFromAPI (success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      const successObject = {
+        status: 200,
+        body: 'Success'
+      };
+      resolve(successObject);
+    } else {
+      const errorObject = {
+        Error: 'The fake API is not working currently'
+      };
+      reject(errorObject);
+    }
+  });
+}
