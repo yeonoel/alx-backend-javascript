ec7700a15e64a345e60cd915db297cfc110f0a9a
yeonoel <yeopevroguinoel@gmail.com>
2022-12-23 22:24:09 +0000

    Catch me if you can!

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,7 @@
 export function handleResponseFromAPI(promise) {
-  if (promise) {
-    console.log("Got a response from the API");
-    return ({ status: 200, body: 'success'});
-  } else {
-    return (new Error(""));
-  }
+    promise.then(() => {
+      console.log("Got a response from the API")
+      return ({ status: 200, body: "success" });
+      })
+      .catch(() => console.log(new Error("")));
 }
