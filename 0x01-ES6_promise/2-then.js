77a49d93b3cd1d917154a21ccab1d0158291ca35
yeonoel <yeopevroguinoel@gmail.com>
2022-12-23 23:42:36 +0000

    Catch me if you can!

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,7 @@
 export default function handleResponseFromAPI(promise) {
+    const object = { status: 200, body: 'success' };
     return promise
-      .then(() => { { status: 200, body: 'success' } })
+      .then(() => object)
       .catch(() => new Error())
-      .finally(() => { console.log("Got a response from the API"); });
+      .finally(() => { console.log('Got a response from the API'); });
 }
