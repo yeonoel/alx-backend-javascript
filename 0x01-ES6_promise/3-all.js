b67a6ffc2922733bbdb7f3e1cc0db253e80b56ee
yeonoel <yeopevroguinoel@gmail.com>
2022-12-24 04:07:21 +0000

    Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,15 +1,14 @@
-import { uploadPhoto, createUser } from "./utils";
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  let photo = uploadPhoto();
-  let user = createUser();
+  const photo = uploadPhoto();
+  const user = createUser();
   let lastName;
   let firstName;
- 
   return Promise.all([photo, user]).then((values) => {
-    let body = values[0].body;
+    const body = values[0].body;
     lastName = values[1].lastName;
     firstName = values[1].firstName;
     console.log(`${body} ${firstName} ${lastName}`);
-  }).catch(() => { console.log("Signup system offline"); });
+  }).catch(() => { console.log('Signup system offline'); });
 }
