e04383b1ba0ecdcb167d4f2b856104266f12b97d
yeonoel <yeopevroguinoel@gmail.com>
2022-12-27 20:59:52 +0000

    Reject the promises

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,6 +1,5 @@
 export default function uploadPhoito(filename) {
-  const fileName = filename;
-  const error = new Error(`${fileName} cannot be processed`);
-  const promiseRejected =  Promise.reject(error);
+  const error = new Error(`${filename} cannot be processed`);
+  const promiseRejected = Promise.reject(error);
   return promiseRejected.then();
 }
