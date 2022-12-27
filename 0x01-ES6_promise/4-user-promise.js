047a0b3cd3da6507a9b287933970a6cad2b35b15
yeonoel <yeopevroguinoel@gmail.com>
2022-12-27 15:11:13 +0000

    Simple promise

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,5 +1,5 @@
 export default function signUpUser(firstName, lastName) {
-  const object = { firstName: firstName, lastName: lastName };
+  const object = { this.firstName: this.firstName, lastName: lastName };
   const resolvedPromise = Promise.resolve(object);
   return resolvedPromise.then((value) => value);
 }
