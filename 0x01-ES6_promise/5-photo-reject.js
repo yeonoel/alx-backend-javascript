0d3a094922c490b1c3f53ca3432d8f6e52efd5ce
yeonoel <yeopevroguinoel@gmail.com>
2022-12-27 19:34:22 +0000

    Reject the promises

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,8 +1,7 @@
 export default function uploadPhoto(filename) {
   const error = new Error(`${fileName} cannot be processed`);
-  const promiseRejected =  Promise.reject(error));
+  const promiseRejected =  Promise.reject(error);
   return promiseRjected.then((null, reject) => {
     reject();
   });
-
 }
