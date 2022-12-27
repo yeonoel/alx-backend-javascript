9fdd61912660170634f6a5520053c79088c9cea5
yeonoel <yeopevroguinoel@gmail.com>
2022-12-27 21:27:08 +0000

    signUpUser

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,8 +2,8 @@ import signUpUser from '4-user-promise';
 import uploadPhoto from '5-photo-reject';
 
 export default function handleProfileSignup(firstname, lastname, filename) {
-  const userPromise = signUpUser();
-  const photoRejected = uploadPhoto();
+  const userPromise = signUpUser(firstname, lastname);
+  const photoRejected = uploadPhoto(filename);
   const promises = [userPromise, photoRejected];
   const array = Promise.allSettled(promises);
   return array.then();
