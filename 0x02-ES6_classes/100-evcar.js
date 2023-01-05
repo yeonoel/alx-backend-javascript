0e0c2f0300978ef301bb076cf7a0e98f47c2c783
yeonoel <yeopevroguinoel@gmail.com>
2023-01-05 18:01:25 +0000

    EVCar

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,12 +1,12 @@
-import car from '10-car';
+import Car from '10-car';
 
-export default class EVCar extands Car {
+export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
     this._range = range;
   }
 
-  // mothod 
+  // mothod
   cloneCar() {
     return new Car[Symbol.species]();
   }
