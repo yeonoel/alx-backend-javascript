ee1bb7470302bc6517fc629a51c3fe655579ef8f
yeonoel <yeopevroguinoel@gmail.com>
2023-01-05 11:47:38 +0000

    Pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,30 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, Currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+  //amount
+  get amount() {
+    return this._amount;
+  }
+  set amount(value) {
+    this._amount = value;
+  }
+
+  //currency
+  get currency() {
+    return this._currency;
+  }
+  set currency(value) {
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this.currency.displayFullCurrency()}`;
+  }
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
