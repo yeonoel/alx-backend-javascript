15380edee4970e53eeaa7b13eaff1d7523ea7952
yeonoel <yeopevroguinoel@gmail.com>
2023-01-05 00:54:34 +0000

    Update

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -3,20 +3,25 @@ export default class Currency {
     this.code = code;
     this.name = name;
   }
+
   // code
   get code() {
     return this._code;
   }
+
   set code(value) {
     this._code = value;
   }
+
   // name
   get name() {
     return this._name;
   }
+
   set name(value) {
     this._name = value;
   }
+
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
   }
