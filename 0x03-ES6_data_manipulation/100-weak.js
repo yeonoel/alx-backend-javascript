637cbfc3f9ac473e16e51cdc04b4b37909201c43
yeonoel <yeopevroguinoel@gmail.com>
2023-01-06 19:40:39 +0000

    Weak link data structure

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -3,8 +3,7 @@ let count = 1;
 
 export function queryAPI(endpoint) {
   weakMap.set(endpoint, count);
-  count++;
-
+  count += 1;
   const numberOfQueries = weakMap.get(endpoint);
   if (numberOfQueries >= 5) {
     throw new Error('Endpoint load is high');
