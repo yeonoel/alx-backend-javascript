ba419fee5b02926d4a5455bf6a92171d08da3ef2
yeonoel <yeopevroguinoel@gmail.com>
2023-01-06 17:14:02 +0000

    Combine

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,3 +1,3 @@
 export default function updateStudentGradeByCity(listStudents, city, newGrades) {
-  return listStudents.filtre((elem) => elem.location === city).map((elem) => ({ ...elem, grade: newGrades.filtre((grad) => grad.studentId === elem.id).map((grad) => grad.grade)[0] || 'N/A' }));
+  return listStudents.filter((elem) => elem.location === city).map((elem) => ({ ...elem, grade: newGrades.filtere((grad) => grad.studentId === elem.id).map((grad) => grad.grade)[0] || 'N/A' }));
 }
