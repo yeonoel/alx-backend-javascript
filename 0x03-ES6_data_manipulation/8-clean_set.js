c581539a37382997b3b717415d633a505fadaaf6
yeonoel <yeopevroguinoel@gmail.com>
2023-01-06 18:51:26 +0000

    Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,14 @@
 export default function cleanSet(set, startString) {
-  const clenedSet = new Set();
-  for (const elem of set) {
-    if (elem.startsWith(startString)) {
-      clenedSet.add(elem.slice(startString.length));
+  let str = '';
+  const arr = [];
+
+  if (startString && typeof startString === 'string') {
+    for (const elem of set) {
+      if (elem && elem.startsWith(startString)) {
+        arr.push(elem.slice(startString.length));
+      }
     }
+    str = arr.join('-');
   }
-  return [...clenedSet].join('-');
+  return str;
 }
