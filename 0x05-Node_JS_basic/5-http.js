df58e2dfe21aa634c95eec55392e00f9a030e157
yeonoel <yeopevroguinoel@gmail.com>
2023-02-23 14:32:19 +0000

    Update

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -2,7 +2,9 @@ const http = require('http');
 
 const args = process.argv.slice(2);
 const countStudents = require('./3-read_file_async');
+
 const DATABASE = args[0];
+
 const hostname = '127.0.0.1';
 const port = 1245;
 
@@ -11,6 +13,7 @@ const app = http.createServer(async (req, res) => {
   res.setHeader('Content-Type', 'text/plain');
 
   const { url } = req;
+  
   if (url === '/') {
     res.write('Hello Holberton School!');
   } else if (url === '/students') {
