ac9316120be1728eb42257e2be1a1511165cd383
yeonoel <yeopevroguinoel@gmail.com>
2023-02-24 14:33:01 +0000

    Update

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,24 +1,22 @@
 const express = require('express');
 
-const args = process.argv.slice(2);
+const DATABASE = process.argv[2];
 const countStudents = require('./3-read_file_async');
-
-const DATABASE = args[0];
 
 const app = express();
 const port = 1245;
 
 app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
+  res.send(200, 'Hello Holberton School!');
 });
 
 app.get('/students', async (req, res) => {
   const message = 'This is list of our students\n';
   try {
     const students = await countStudents(DATABASE);
-    res.send(`${message}${students.join('\n')}`);
+    res.send(200, `${message}${students.join('\n')}`);
   } catch (error) {
-    res.send(`${message}${error.message}`);
+    res.send(500, `${message}${error.message}`);
   }
 });
 
