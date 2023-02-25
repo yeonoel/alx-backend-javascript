2d35a73a1c8ab93a4b194e267cf9534a2a6882a3
yeonoel <yeopevroguinoel@gmail.com>
2023-02-25 18:55:34 +0000

    Update

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,34 +1,25 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe("calculateNumber function:", function() {
-    describe("Calculates two integers:", function() {
-        it('Returns 4.', function() {
-            assert.strictEqual(calculateNumber(1, 3), 4);
-        });
-    });
 
-    describe("calculates integer and float:", function() {
-        it("returns 5", function() {
-            assert.strictEqual(calculateNumber(1, 3.7), 5);
-        });
-    });
+describe('SUM', function() {
+  it('Returns 4.', function() {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+  });
 
-    describe("calculates two floats w/border line:", function() {
-        it("returns 6", function() {
-            assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-        });
-    });
+  it('returns 5', function() {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
 
-    describe("calculates two floats:", function() {
-        it("returns 5", function() {
-            assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-        });
-    });
+  it('returns 6', function() {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+ 
+  it('returns 5', function() {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
 
-    describe("calculates two floats rounded down:", function() {
-        it("retuns 4", function() {
-            assert.strictEqual(calculateNumber(1.2, 3.1), 4);
-        });
-    });
+  it('retuns 4', function() {
+    assert.strictEqual(calculateNumber(1.2, 3.1), 4);
+  });
 });
