050a969a1e0f2c8132b50b65a3d08b70172736ae
yeonoel <yeopevroguinoel@gmail.com>
2023-02-25 19:34:16 +0000

    Combining descriptions

---
+++ 0x06-unittests_in_js/1-calcul.js
@@ -0,0 +1,21 @@
+function calculateNumber(type, a, b) {
+  const numberOne = Math.round(a);
+  const numberTwo = Math.round(b);
+
+  if (type === 'SUM') {
+    return numberOne + numberTwo;
+  }
+
+  if (type === 'SUBTRACT') {
+    return numberTwo - numberOne;
+  }
+
+  if (type === 'DIVIDE') {
+    if (numberTwo === 0) {
+      return 'Error';
+    }
+    return numberOne / numberTwo;
+  }
+}
+
+module.exports = calculateNumber;
---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,69 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
+
+
+describe('SUM', function() {
+  it('Returns 4.', function() {
+    assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
+  });
+
+  it('should return the sum of two rounded numbers', function() {
+    assert.strictEqual(calculateNumber('SUM', 1, 3.7), 5);
+  });
+
+  it('returns 6', function() {
+    assert.strictEqual(calculateNumber('SUM', 1.5, 3.7), 6);
+  });
+
+  it('should return the sum of two rounded numbers', function() {
+    assert.strictEqual(calculateNumber('SUM', 1.2, 3.7), 5);
+  });
+
+  it('retuns 4', function() {
+    assert.strictEqual(calculateNumber('SUM', 1.2, 3.1), 4);
+  });
+});
+
+describe('SUBTRACT', function() {
+  it('should return the SUBTRACT of two rounded numbers', function() {
+    assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), 4);
+  });
+
+  it('should return the SUBTRACT of two rounded numbers', function() {
+    assert.strictEqual(calculateNumber('SUBTRACT', 1, 3.7), 3);
+  });
+
+  it('should return the SUBTRACT of two rounded numbers', function() {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 2);
+  });
+
+  it('should return the SUBTRACT of two rounded numbers', function() {
+    assert.strictEqual(calculateNumber('SUBTRACT', 1.2, 3.7), 2);
+  });
+
+  it('should return the SUBTRACT of two rounded numbers', function() {
+    assert.strictEqual(calculateNumber(1.2, 3.1), 2);
+  });
+});
+
+describe('DIVIDE', function() {
+  it('should return Error of numberOne DIVIDE 0', function() {
+    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+  });
+
+  it('should return the DIVIDE of two rounded numbers', function() {
+    assert.strictEqual(calculateNumber('DIVIDE', 5, 3.7), 1.25);
+  });
+
+  it('should return the DIVIDE of two rounded numbers', function() {
+    assert.strictEqual(calculateNumber('DIVIDE', 1.5, 3.7), 0.5);
+  });
+
+  it('should return the DIVIDE of two rounded numbers', function() {
+    assert.strictEqual(calculateNumber('DIVIDE', 0, 3.7), 0);
+  });
+
+  it('should return the DIVIDE of two rounded numbers', function() {
+    assert.strictEqual(calculateNumber('DIVIDE', 4.1, 1.6), 2);
+  });
+});
