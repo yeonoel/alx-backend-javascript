6edb4ab3b1f9c7eaf8a28f247d43b3810fcfa984
yeonoel <yeopevroguinoel@gmail.com>
2023-02-25 20:18:38 +0000

    Combining descriptions

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,27 +1,28 @@
-const assert = require('assert');
-const calculateNumber = require('./0-calcul');
+const assert  = require('assert');
+const calculateNumber = require('./1-calcul');
 
+describe('Test advanced calculateNumber()', function() {
+    describe('Tests SUM ():', function(){
+        it('Returns 6', function() {
+            assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6)
+        })
+    })
 
-describe('SUM', function() {
-  it('should return the sum of two rounded numbers.', function() {
-    assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
-  });
-});
+    describe('Tests SUBTRACT ():', function(){
+        it('Return -4', function() {
+            assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4)
+        })
+    })
 
-describe('SUBTRACT', function() {
-  it('should return the SUBTRACT of two rounded numbers', function() {
-    assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
-  });
-});
+    describe('Tests DIVIVDE ():', function(){
+        it('return 0.2', function() {
+            assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2)
+        })
+    })
 
-
-describe('DIVIDE', function() {
-  it('shouls return Error for divion by 0', function() {
-    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
-  });
-
-  it('should return the DIVIDE of two rounded numbers', function() {
-    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
-  });
-});
-
+    describe('Tests DIVIVDE () with 0:', function(){
+        it('return Error', function() {
+            assert.strictEqual(calculateNumber('DIVIDE', 8.3, 0), 'Error')
+        })
+    })
+})
