a5efe67772f2267cf08219033ef9d6731cf916d1
yeonoel <yeopevroguinoel@gmail.com>
2023-02-27 12:18:24 +0000

    Spies

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,19 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+
+describe('Spy', function() {
+  it('Ensure math is the same', function() => {
+    const spyUtils = sinon.spy(Utils, 'calculateNumber');
+    const spyConsole = sinon.spy(console, 'log');
+    
+    sendPaymentRequestToApi(100, 20);
+    expect(spyUtils.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    expect(spyConsole.calledOnceWithExactly('The total is : 120')).to.be.true;
+    
+    spyUtils.restore();
+    spyConsole.restore();
+  });
+});
---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,21 @@
+const Utils = {
+  calculateNumber(type, a, b) {
+    const numberOne = Math.round(a);
+    const numberTwo = Math.round(b);
+
+    if (type === 'SUBTRACT') {
+      return numberOne - numberTwo;
+    }
+
+    if (type === 'DIVIDE') {
+      if (numberTwo === 0) {
+        return 'Error';
+      }
+     return numberOne / numberTwo;
+    }
+
+    return numberOne + numberTwo;
+  }
+}
+
+module.exports Utils;
