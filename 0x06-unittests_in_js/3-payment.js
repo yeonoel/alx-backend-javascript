6fa3016cfb93b3676fb28c2365156043c6b8ec56
yeonoel <yeopevroguinoel@gmail.com>
2023-02-27 12:44:55 +0000

    Update

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,8 +1,20 @@
+const sinon = require('sinon');
+var { expect } = require('chai');
+
+const sendPaymentRequestToApi = require('./3-payment');
 const Utils = require('./utils');
 
-function sendPaymentRequestToApi(totalAmount, totalShipping) {
-  const total = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-  console.log(`The total is: ${total}`);
-}
+describe('Spy', function () {
+    it('Ensures math is the same.', () => {
+        const spyUtils = sinon.spy(Utils, 'calculateNumber');
+        const spyConsole = sinon.spy(console, 'log');
 
-module.exports = sendPaymentRequestToApi;
+        sendPaymentRequestToApi(100, 20);
+
+        expect(spyUtils.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+        expect(spyConsole.calledOnceWithExactly('The total is: 120')).to.be.true;
+
+        spyUtils.restore();
+        spyConsole.restore();
+    });
+});
--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,21 +1,21 @@
 const Utils = {
-  calculateNumber(type, a, b) {
-    const numberOne = Math.round(a);
-    const numberTwo = Math.round(b);
-
-    if (type === 'SUBTRACT') {
-      return numberOne - numberTwo;
+    calculateNumber(type, a, b) {
+        const first_num = Math.round(a);
+        const second_num = Math.round(b);
+    
+        if (type === 'SUBTRACT') {
+            return first_num - second_num; 
+        }
+        
+        if (type === 'DIVIDE') {
+            if (second_num === 0) {
+                return "Error";
+            }
+            return first_num / second_num;
+        }
+        
+        return first_num + second_num;
     }
-
-    if (type === 'DIVIDE') {
-      if (numberTwo === 0) {
-        return 'Error';
-      }
-     return numberOne / numberTwo;
-    }
-
-    return numberOne + numberTwo;
-  }
-}
-
-module.exports Utils;
+};
+    
+module.exports = Utils;
