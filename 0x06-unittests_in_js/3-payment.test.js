4ee93822980b7d5856c1fba955ce6b617a5f2e39
yeonoel <yeopevroguinoel@gmail.com>
2023-02-27 12:40:52 +0000

    Spies

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -5,13 +5,13 @@ const sendPaymentRequestToApi = require('./3-payment');
 const Utils = require('./utils');
 
 describe('Spy', function() {
-  it('Ensure math is the same', function() => {
+  it('Ensures math is the same', function() => {
     const spyUtils = sinon.spy(Utils, 'calculateNumber');
     const spyConsole = sinon.spy(console, 'log');
     
     sendPaymentRequestToApi(100, 20);
     expect(spyUtils.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
-    expect(spyConsole.calledOnceWithExactly('The total is : 120')).to.be.true;
+    expect(spyConsole.calledOnceWithExactly('The total is: 120')).to.be.true;
     
     spyUtils.restore();
     spyConsole.restore();
